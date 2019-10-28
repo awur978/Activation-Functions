3bd72fda22345f66e8e0e6d8fe6fe7c1d1d6075a
Adedamola Wuraola <32375311+awur978@users.noreply.github.com>
2019-10-28 18:25:17 +1300

    Include comments

--- sq_rbf.m
+++ sq_rbf.m
@@ -1,3 +1,5 @@
+%A replacement simple and computationally efficient Gaussian Radial Basis function
+%Uses: RBF Neural Networks
 function a = sq_rbf(n,param)
 			m = abs(n); 
 			a = (m < 1) .* (1 - m.^2/2)             + ...
