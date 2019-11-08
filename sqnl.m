e242eff20505b5a598bc62fc54bf1f6de4d31476
Adedamola Wuraola <32375311+awur978@users.noreply.github.com>
2019-11-09 09:37:08 +1300

    Update sqnl.m

--- sqnl.m
+++ sqnl.m
@@ -1,5 +1,6 @@
 %Computationally efficient square based nonlinearity
 %to replace the popular computationally expensive Tanh
+
 function a = sqnl(n,param)
 a=zeros(size(n));
 
@@ -8,7 +9,6 @@ n(idx) = 2;
 
 idx = find(n < -2);
 n(idx) = -2;
-
 
 idx = find(n >= 0);
 a(idx) = n(idx) - n(idx).^2/4;
