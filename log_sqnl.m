3db7547f2a2e312ff3b280612b03c76ee3b69b8a
Adedamola Wuraola <32375311+awur978@users.noreply.github.com>
2019-11-28 08:45:30 +1300

    Update log_sqnl.m

--- log_sqnl.m
+++ log_sqnl.m
@@ -9,13 +9,11 @@ n(idx) = 2;
 idx = find(n < -2);
 n(idx) = -2;
 
-
 idx = find(n >= 0);
 y(idx) = ((n(idx) - n(idx).^2/4)*0.5)+0.5;
 
 idx = find(n < 0);
 y(idx) = ((n(idx) + n(idx).^2/4)*0.5)+0.5;
-
 
 
 % idx = find(x > 2);
